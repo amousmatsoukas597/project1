0979ded515fa9bbe2a2d42caa4cc2038a6ad32b0
amousmatsoukas597 <amousmatsoukas597@gmail.com>
2025-03-09 09:01:29 +0000

    Add 4.cpp

---
+++ src/4.cpp
@@ -0,0 +1,20 @@
+// Random C++ Code
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name = "John Doe";
+    int age = 30;
+    double height = 180.0;
+    bool isAlive = true;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Alive: " << (isAlive ? "Yes" : "No") << endl;
+
+    return 0;
+}
