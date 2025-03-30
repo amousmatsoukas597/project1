96f1f2902e0fa6621b86e92d7cca680855a865b6
amousmatsoukas597 <amousmatsoukas597@gmail.com>
2025-03-30 21:34:52 +0000

    Add 19.cpp

---
+++ src/19.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int sum = a + b; // Addition of two integers
+    cout << "The sum is: " << sum << endl; // Print the result
+
+    return 0; // Return 0 to indicate successful execution
+}
