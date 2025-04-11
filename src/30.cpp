0f5b48e5a778d2c90e8f28843cfd7ad04f6c408a
amousmatsoukas597 <amousmatsoukas597@gmail.com>
2025-04-11 12:29:13 +0000

    Add 30.cpp

---
+++ src/30.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 5;
+    if(num > 0) {
+        cout << "Number is greater than 0" << endl;
+    } else {
+        cout << "Number is not greater than 0" << endl;
+    }
+}
