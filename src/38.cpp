75791787ef9ab2829fb01d1717f38020eced2ffa
amousmatsoukas597 <amousmatsoukas597@gmail.com>
2025-04-11 12:45:25 +0000

    Add 38.cpp

---
+++ src/38.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    if (n > 0) {
+        cout << "The number is positive" << endl;
+    } else {
+        cout << "The number is not positive." << endl;
+    }
+    
+    return 0;
+}
